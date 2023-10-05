b0685afc40992136ebdb67699d4842825ccb1333
Pierpaolo Vanni <pierpaolo.vanni@docplanner.com>
2023-10-05 16:22:36 +0200

    accordion

--- assets/ik_accordion.js
+++ assets/ik_accordion.js
@@ -1,11 +1,11 @@
-;(function ( $, window, document, undefined ) {
- 	
+; (function ($, window, document, undefined) {
+
 	var pluginName = 'ik_accordion',
 		defaults = { // set default parameters
 			autoCollapse: false,
 			animationSpeed: 200
 		};
-	 
+
 	/**
 	 * @constructs Plugin
 	 * @param {Object} element - Current DOM element from selected collection.
@@ -13,52 +13,55 @@
 	 * @param {boolean} options.autoCollapse - Automatically collapse inactive panels.
 	 * @param {number} options.animationSpeed - Panel toggle speed in milliseconds.
 	 */
-	function Plugin( element, options ) {
-		
+	function Plugin(element, options) {
+
 		this._name = pluginName;
 		this._defaults = defaults;
 		this.element = $(element);
-		this.options = $.extend( {}, defaults, options) ; // override default parameters if setup object is present
-		
+		this.options = $.extend({}, defaults, options); // override default parameters if setup object is present
+
 		this.init();
 	}
-	
+
 	/** Initializes plugin. */
 	Plugin.prototype.init = function () {
-		
+
 		var id, $elem, plugin;
-		
+
 		id = 'acc' + $('.ik_accordion').length; // create unique id
 		$elem = this.element;
 		plugin = this;
-		
+
 		$elem.attr({
 			'id': id
 		}).addClass('ik_accordion');
-			
-		this.headers = $elem.children('dt').each(function(i, el) {
+
+		this.headers = $elem.children('dt').each(function (i, el) {
 			var $me, $btn;
-			
+
 			$me = $(el);
 			$btn = $('<div/>').attr({
-          'id': id + '_btn_' + i
-        })
-        .addClass('button')
-        .html($me.html())
-        .on('click', {'plugin': plugin}, plugin.togglePanel);
-        
+				'id': id + '_btn_' + i
+			})
+				.addClass('button')
+				.html($me.html())
+				.on('click', { 'plugin': plugin }, plugin.togglePanel)
+				.on('keydown', { 'plugin': plugin }, plugin.togglePanel);
+
 			$me.empty().append($btn); // wrap content of each header in an element with role button
 		});
-		
-		this.panels = $elem.children('dd').each(function(i, el) {
+
+		$elem.on('keydown', { plugin: plugin }, plugin.togglePanel);
+
+		this.panels = $elem.children('dd').each(function (i, el) {
 			var $me = $(this), id = $elem.attr('id') + '_panel_' + i;
 			$me.attr({
 				'id': id
 			});
 		}).hide();
-		
+
 	};
-	
+
 	/** 
 	 * Toggles accordion panel.
 	 *
@@ -67,45 +70,45 @@
 	 * @param {object} event.data.plugin - Reference to plugin.
 	 */
 	Plugin.prototype.togglePanel = function (event) {
-		
 		var plugin, $elem, $panel, $me, isVisible;
-		
+
 		plugin = event.data.plugin;
 		$elem = $(plugin.element);
 		$me = $(event.target);
 		$panel = $me.parent('dt').next();
-		
+
 		isVisible = !!$panel.is(':visible');
+		$me.parent('dt').attr({'aria-expanded': isVisible});
 		$panel.slideToggle({ duration: plugin.options.animationSpeed });
-		
-		if(plugin.options.autoCollapse) { // collapse all other panels
-			
-			plugin.headers.each(function(i, el) {
-				var $hdr, $btn; 
-				
+
+		if (plugin.options.autoCollapse) { // collapse all other panels
+
+			plugin.headers.each(function (i, el) {
+				var $hdr, $btn;
+
 				$hdr = $(el);
 				$btn = $hdr.find('.button');
-				
-				if($btn[0] != $(event.currentTarget)[0]) { 
+
+				if ($btn[0] != $(event.currentTarget)[0]) {
 					$btn.removeClass('expanded');
 					$hdr.next().slideUp(plugin.options.animationSpeed);
 				}
 			});
-			
+
 		}
 	};
-	
-	$.fn[pluginName] = function ( options ) {
-		
+
+	$.fn[pluginName] = function (options) {
+
 		return this.each(function () {
-			
-			if ( !$.data(this, pluginName )) {
-				$.data( this, pluginName,
-				new Plugin( this, options ));
+
+			if (!$.data(this, pluginName)) {
+				$.data(this, pluginName,
+					new Plugin(this, options));
 			}
-			
+
 		});
-		
+
 	}
- 
-})( jQuery, window, document );
+
+})(jQuery, window, document);
